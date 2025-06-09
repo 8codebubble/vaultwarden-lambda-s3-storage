62d1967299856bb37db2baa207a75292fb3e0baf
8codebubble <8codebubble@gmail.com>
2025-06-10 05:52:38 +0900

    syntax stuff

--- lambda-entrypoint.sh
+++ lambda-entrypoint.sh
@@ -5,7 +5,9 @@ set -e
 echo "Starting Vaultwarden Lambda container..."
 
 # Create the directory (if necessary) so that it's available when the container starts.
-mkdir -p ${VAULTWARDEN_DATA_DIR}
+echo $VAULTWARDEN_DATA_DIR
+echo ${VAULTWARDEN_DATA_DIR}
+mkdir ${VAULTWARDEN_DATA_DIR}
 
 # Restore SQLite database from S3 if available
 litestream restore -if-replica-exists /vaultwarden/data/db.sqlite3
