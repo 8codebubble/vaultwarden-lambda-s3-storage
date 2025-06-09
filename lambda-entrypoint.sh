f4dec94d967386f2fc7abff9ba2611edcd53b1ab
8codebubble <8codebubble@gmail.com>
2025-06-10 06:03:11 +0900

    update location for litestream

--- lambda-entrypoint.sh
+++ lambda-entrypoint.sh
@@ -10,7 +10,7 @@ echo ${VAULTWARDEN_DATA_DIR}
 mkdir ${VAULTWARDEN_DATA_DIR}
 
 # Restore SQLite database from S3 if available
-litestream restore -if-replica-exists /vaultwarden/data/db.sqlite3
+litestream restore -if-replica-exists ${VAULTWARDEN_DATA_DIR}/db.sqlite3
 
 # Start Litestream in background for continuous replication
 litestream replicate -config /etc/litestream.yml &
